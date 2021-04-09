3cfebfb51034840883c371cb057bee9d1f9ce21d
Will Larry <will23larry@gmail.com>
2021-04-09 13:56:52 -0400

    Add tailwind

---
+++ app/javascript/stylesheets/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [
+    "./app/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/javascript/**/*.jsx",
+    "./app/javascript/**/*.tsx",
+    "./app/javascript/**/*.ts",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
