94ded5427b89fbbf358fbeec0ea08defd88a2263
Will Larry <will23larry@gmail.com>
2021-05-11 14:51:38 -0400

    Add ability for user to sign in and out.

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'pg'
 gem 'pg_search'
 gem 'rack-mini-profiler', require: false
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
+gem 'rack-cors'
+gem 'jwt'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise  :database_authenticatable,
+          :registerable,
+          :recoverable,
+          :rememberable,
+          :validatable
 
   has_many :recipes
   has_many :favorites, dependent: :destroy
+
+  def generate_jwt
+    JWT.encode({id: id, exp: 60.days.from_now.to_i}, Rails.application.secrets.secret_key_base)
+  end
 
   def favorite_for(recipe)
     favorites.find_by(recipe_id: recipe.id)
