d135edfa4680d10435394413dc013bb1939c01e2
Will Larry <will23larry@gmail.com>
2021-05-18 21:55:51 -0400

    Change gql type to snake case

--- app/graphql/types/ingredient_type.rb
+++ app/graphql/types/ingredient_type.rb
@@ -4,14 +4,14 @@ module Types
     field :aisle, String, null: true
     field :consistency, String, null: true
     field :name, String, null: true
-    field :nameClean, String, null: true
+    field :name_clean, String, null: true
     field :original, String, null: true
-    field :originalString, String, null: true
-    field :originalClean, String, null: true
+    field :original_string, String, null: true
+    field :original_clean, String, null: true
     field :amount, Integer, null: true
     field :unit, String, null: true
     field :meta, String, null: true
-    field :metaInformation, String, null: true
+    field :meta_information, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
