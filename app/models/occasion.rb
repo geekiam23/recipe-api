3d7fa4ed73d16dde96a6098f5201c627f96afaf4
Will Larry <will23larry@gmail.com>
2021-05-20 11:06:12 -0400

    Add annotate to project

--- app/models/occasion.rb
+++ app/models/occasion.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: occasions
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Occasion < ApplicationRecord
   has_many :occasion_recipes
   has_many :recipes, through: :occasion_recipes
