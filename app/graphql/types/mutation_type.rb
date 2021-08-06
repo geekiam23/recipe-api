446aefa65b089a77c0309ccaad41c120443af96a
Will Larry <will23larry@gmail.com>
2021-08-06 17:20:07 -0400

    Refactor random recipe spec

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,8 @@
 module Types
   class MutationType < Types::BaseObject
-    field :sign_in_mutation, mutation: Mutations::SignInMutation
-    field :sign_out_mutation, mutation: Mutations::SignOutMutation
+    field :create_account_mutation, mutation: Mutations::Auth::CreateAccountMutation
+    field :recipe_favorite_mutation, mutation: Mutations::Recipe::FavoriteMutation
+    field :sign_in_mutation, mutation: Mutations::Auth::SignInMutation
+    field :sign_out_mutation, mutation: Mutations::Auth::SignOutMutation
   end
 end
--- spec/graphql/query/random_recipe_spec.rb
+++ spec/graphql/query/random_recipe_spec.rb
@@ -16,18 +16,36 @@ RSpec.describe 'query random recipe', type: :graphql do
     GQL
   end
 
-  it 'returns the correct title' do
-    user = create(:user)
-    recipe = create(:recipe, user_id: user.id, spoonacular_id: 1)
-    
-    result = execute_graphql(
+  let(:user) do
+    create(:user)
+  end
+
+  let(:recipe) do
+    create(:recipe, user_id: user.id, spoonacular_id: 1)
+  end
+
+  let(:result) do
+    execute_graphql(
       query,
       variables: { id: recipe.id.to_s, userId: user.id.to_s }
     )
+  end
 
+  it 'returns the correct title' do
+    binding.pry
     expect(result['data']['getRandomRecipe']['title']).to eq 'Gumbo'
+  end
+
+  it 'returns the correct servings' do
+    binding.pry
     expect(result['data']['getRandomRecipe']['servings']).to eq '20'
+  end
+
+  it 'returns the correct summary' do
     expect(result['data']['getRandomRecipe']['summary']).to eq 'Best Recipe Ever' 
+  end
+
+  it 'returns the correct instructions' do
     expect(result['data']['getRandomRecipe']['instructions']).to eq 'Have Fun!'
   end
 end
